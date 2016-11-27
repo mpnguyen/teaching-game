9d919735b7771af32baae5717523c2a69491a03d
mpnguyen <minhphu.it@gmail.com>
2016-11-27 13:17:19 +0700

    - Add model package, question

---
+++ front-end/app/models/package.model.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by mp_ng on 11/26/2016.
+ */
+
+export class Package {
+    id: string;
+    title: string;
+}
---
+++ front-end/app/models/question.model.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by mp_ng on 11/26/2016.
+ */
+export class Question {
+    id: string;
+    question: string;
+    image: string;
+    answers: string[];
+    correct: number;
+}
